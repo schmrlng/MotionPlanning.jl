e4c020f7fcb7180e2d7c2e956bdb38eb1eb0bde6
Ed Schmerling <ednerd@gmail.com>
2016-08-02 16:07:59 -0700

    Fixed @any, @all to accomodate new comprehension/generator semantics; these macros are probably unnecessary going forward given that generators now exist in julia

--- src/utilities/utils.jl
+++ src/utilities/utils.jl
@@ -4,9 +4,10 @@ macro any(comp)
     if !isa(comp, Expr) || (comp.head != :comprehension)
         error("@any may only precede a comprehension")
     end
-    loop = Expr(:for, esc(comp.args[2]),
+    compgen = comp.args[1]
+    loop = Expr(:for, esc(compgen.args[2]),
                           quote
-                              if $(esc(comp.args[1]))
+                              if $(esc(compgen.args[1]))
                                   result = true
                                   break
                               end
@@ -22,9 +23,10 @@ macro all(comp)
     if !isa(comp, Expr) || (comp.head != :comprehension)
         error("@all may only precede a comprehension")
     end
-    loop = Expr(:for, esc(comp.args[2]),
+    compgen = comp.args[1]
+    loop = Expr(:for, esc(compgen.args[2]),
                           quote
-                              if !$(esc(comp.args[1]))
+                              if !$(esc(compgen.args[1]))
                                   result = false
                                   break
                               end
